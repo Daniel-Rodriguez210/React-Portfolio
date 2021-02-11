72d85fb346bd4a2f14aa3a243e46b394a6eb02be
Daniel Rodriguez <drod18@hotmail.com>
2021-02-10 19:15:17 -0600

    Added additional card object information to have the descriptions on the home page included as well as the links for the demo and github.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
-import CardInfo from './CardInfo';
+import CardInfo from '../components/CardInfo';
 
 function Card(props) {
 
-    return(
-        <div className= "d-inline-block d-card" onClick={(e) => props.click(props.item)}>
-            <img className="d-card-image" src={props.item.imgSrc} alt={props.item.imgSrc} />
-            { props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link}/>}
-
-
+    return (
+        <div>
+            <div className="d-inline-block d-card" onClick={(e) => props.click(props.item)}>
+                <img className="d-card-image" src={props.item.imgSrc} alt={props.item.imgSrc} />
+                {props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} technologiesUsed={props.item.technologiesUsed} link={props.item.link} demo={props.item.demo}/>}
+            </div>
+           
         </div>
     );
 
