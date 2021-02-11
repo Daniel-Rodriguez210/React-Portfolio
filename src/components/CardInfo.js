d99f23e647e681d4d39763abde2743ec49fc7e60
Daniel Rodriguez <drod18@hotmail.com>
2021-02-10 19:16:02 -0600

    Added the additional information needed from the objects to have bigger descriptions and the demo and github links included.

--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -12,11 +12,15 @@ function CardInfo(props) {
     return (
         <div>
 
-            <animated.div className="d-card-info" style={style}>
-                <p className="d-card-title">{props.title}</p>
-                <p className="d-card-sub-title">{props.subTitle}</p>
-                <a href={props.link} target="_blank" rel="noopener noreferrer">View</a>
+            <animated.div className="g-card-info" style={style}>
+                <p className="cardy g-card-title">{props.title}</p>
+                <p className="cardy g-card-sub-title">{props.subTitle}</p>
+                <p className="cardy g-card-sub-title">{props.technologiesUsed}</p>
+                <a className="links" href={props.link} target="_blank" rel="noopener noreferrer">Github </a>
+                <br/>
+                <a className="links" href={props.demo} target="_blank" rel="noopener noreferrer">Demo</a>
             </animated.div>
+            
 
         </div>
     )
