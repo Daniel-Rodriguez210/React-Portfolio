e9c60770167ae988fcb7afe5db03b9b3bacc43ac
Daniel Rodriguez <drod18@hotmail.com>
2021-02-10 19:18:07 -0600

    cleaned the footer page to stick to the bottom and got rid of the additional name information properly spaced the links as they were bunched

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,18 +6,15 @@ import Col from 'react-bootstrap/Col'
 function Footer() {
 
 return (
-    <footer className="mt-5">
+    <footer className="footer mt-5">
         <Container fluid={true}>
             <Row className="border-top justify-content-between p-3">
-                <Col className="p-0 md={3} sm={12}">
-                Daniel Rodriguez
+                <Col className="p-0 links">
+                    <a className="p-2" href="https://docs.google.com/document/d/1wReEldfwRb5gmopsVun0Wv8mmO5wFa_2IGMPob6-owQ/edit?usp=sharing">Resume</a>
+                    <a className="p-2" href="https://github.com/Daniel-Rodriguez210">Github</a>
+                    <a className="p-2" href="https://www.linkedin.com/in/daniel-rodriguez-73690b1b0/">LinkedIn</a>
                 </Col>
-                <Col className="p-0">
-                    <a href="https://docs.google.com/document/d/1AfchCIHH067YCn695NuDd5N5fg-LWpnTPd9CREj015Q/edit?usp=sharing">Resume</a>
-                    <a href="https://github.com/Daniel-Rodriguez210">Github</a>
-                    <a href="https://www.linkedin.com/in/daniel-rodriguez-73690b1b0/">LinkedIn</a>
-                </Col>
-                <Col className="p-0 d-flex justify-content-end" md={3}>
+                <Col className="madeBy p-0 d-flex justify-content-end" md={3}>
                     This website was made by Daniel Rodriguez.
                 </Col>
             </Row>
