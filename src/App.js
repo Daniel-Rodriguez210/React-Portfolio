c0ef6e7c0248f42cc9383c49f02765f9e63acb79
Daniel Rodriguez <drod18@hotmail.com>
2021-02-10 19:12:55 -0600

    Added the correct css file to link the changes to be made to the pages. I also corrected the landing page to have it be the home page.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import './App.css'
 
 import Footer from './components/Footer'
 import HomePage from './pages/HomePage'
@@ -10,7 +11,7 @@ import AboutPage from './pages/AboutPage'
 import ContactPage from './pages/ContactPage'
 
 class App extends React.Component {
-  
+
   constructor(props) {
     super(props);
     this.state = {
@@ -23,7 +24,7 @@ class App extends React.Component {
       home: {
         title: 'React Portfolio',
         subTitle: 'Projects',
-        text: 'Checkout my projects below'
+        text: 'Checkout my projects below. Click on images for links and description.'
       },
       about: {
         title: 'About Me',
@@ -33,36 +34,36 @@ class App extends React.Component {
       }
     }
   }
-  
-  
-  
+
+
+
   render() {
-  return (
-  <Router>
-<Container className="p-0" fluid={true}>
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}>
 
-<Navbar className="border-bottom" bg="transparent" expand="lg">
-  <Navbar.Brand>Daniel Rodriguez</Navbar.Brand>
-  <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
-  <Navbar.Collapse id="navbar-toggle">
-    <Nav className="ml-auto">
-      <Link className="nav-link" to="/">Home</Link>
-      <Link className="nav-link" to="/about">About</Link>
-      <Link className="nav-link" to="/contact">Contact</Link>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Daniel Rodriguez</Navbar.Brand>
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/React-Portfolio">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
 
-<Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text}/>} />
-<Route path="/about" render={() => <AboutPage title={this.state.about.title} />}/>
-<Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />}/>
+          <Route path="/React-Portfolio" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />}></Route>
+          <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
 
-<Footer/>
+          <Footer />
 
-</Container>
+        </Container>
 
-  </Router>
-  );
+      </Router>
+    );
   }
 }
 export default App;
